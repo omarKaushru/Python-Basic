446f823771b1cad84d07853951999426cd6fe34e
Md. Omar Kaushru <omarkaushru@gmail.com>
2021-02-17 09:50:18 +0600

    Methods

--- DICTIONARIES/main.py
+++ DICTIONARIES/main.py
@@ -44,4 +44,35 @@ new_dic = A.copy()
 new_dic.update(B)
 print(new_dic)
 """
+# Dictionary Methods
+omar = {'Name': 'Md Omar Kaushru','Phone': '01712-166516', 'Job': 'Software Engineer'}
+"""
+# printing values
+for i in omar.values():
+    print(i)
+# printing keys
+for i in omar.keys():
+    print(i)
+# printing both keys and values
+for i in omar.items():
+    print(i)
 
+"""
+# Separating keys and values into the list for the dictionary
+# x = list(omar.keys())
+# y = list(omar.values())
+# print(x)
+# print(y)
+# another technique
+# for x,y in omar.items():
+#    print('Key: '+x+' Value: '+y)
+
+# use of in keyword
+# print('Name' in omar)
+# print('Age' in omar)
+# print('Md Omar Kaushru' in omar.values())
+# print('Age' in omar.keys())
+
+# the get() method
+print(str(omar.get('Name', 'DEFAULT')))
+print(str(omar.get('Age', 'DEFAULT')))
