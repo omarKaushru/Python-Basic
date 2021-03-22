ec81ed1c92f00841220faed15ba81e84428e79c9
Md. Omar Kaushru <omarkaushru@gmail.com>
2021-03-23 00:25:06 +0600

    Python Multiple Inheritance

--- pythonOOP_SecondPart/main.py
+++ pythonOOP_SecondPart/main.py
@@ -1,3 +1,4 @@
+"""
 class Robot:
     def __init__(self, name, version):
         self.name = name
@@ -23,7 +24,7 @@ class HouseBot(Robot):
         self.area_covered = area_covered
 
     def clean(self):
-        if self.area_covered>0:
+        if self.area_covered > 0:
             print(f'{self.name} is cleaning the house!')
             self.area_covered -= 30
             if self.area_covered < 0:
@@ -53,16 +54,64 @@ hBot.move_backward()
 # schoolBot.move_right()
 hBot.clean()
 hBot.halt()
-# print(help(Robot))
 
-# print(help(HouseBot))
 
-# print(issubclass(HouseBot, Robot))
-# print(issubclass(Robot, HouseBot))
-# print(issubclass(Robot, object))
-#
+print(help(Robot))
+
+print(help(HouseBot))
+
+print(issubclass(HouseBot, Robot))
+print(issubclass(Robot, HouseBot))
+print(issubclass(Robot, object))
+
 
 print(isinstance(hBot, Robot))
 print(isinstance(hBot, HouseBot))
 print(isinstance(hBot, object))
 print(isinstance(Robot, object))
+"""
+
+
+# Python Multiple Inheritance
+
+class Dancer:
+    def __init__(self, style):
+        self.style = style
+
+    def dance(self):
+        print(f'Dancing in {self.style} style')
+
+
+class Singer:
+    def __init__(self, genre):
+        self.genre = genre
+
+    def sing(self):
+        print(f'Singing {self.genre} music')
+
+
+class Artist:
+    def __init__(self, painting_material):
+        self.painting_material = painting_material
+
+    def paint(self):
+        print(f'Painting with {self.painting_material}')
+
+
+class SuperHuman(Dancer, Singer, Artist):
+    def __init__(self, style, genre, painting_material, sport):
+        Dancer.__init__(self, style)
+        Singer.__init__(self, genre)
+        Artist.__init__(self, painting_material)
+        self.sport = sport
+
+    def play_sport(self):
+        print(f'Playing {self.sport}')
+
+
+s = SuperHuman('Hip-Hop', 'Classical', 'Acrylic', 'Football')
+print(s.style)
+print(s.genre)
+print(s.painting_material)
+print(s.sport)
+s.sing()
